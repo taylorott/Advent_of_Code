9f1c6d31bae1e5070b256506f07a7c20f3095702
taylorott <orion.thomas.taylor@gmail.com>
2023-12-18 17:11:30 -0500

    Day18 Solution

--- src/Year_2023/Day18/Solution.py
+++ src/Year_2023/Day18/Solution.py
@@ -28,191 +28,10 @@ def parse_input01(fname):
     return data
 
 direction_dict = {'U':(-1,0),'D':(1,0),'L':(0,-1),'R':(0,1)}
-
-def turn_left(dir_in):
-    return (-dir_in[1],dir_in[0])
-def turn_right(dir_in):
-    return (dir_in[1],-dir_in[0])
-
-def compute_interior_directions(command_list):
-    winding_number = 0
-
-    for i in range(len(command_list)):
-  
-        direction0 = direction_dict[command_list[i][0]]
-        direction1 = direction_dict[command_list[(i+1)%len(command_list)][0]]
-
-        if direction1 == turn_left(direction0):
-            winding_number+=1
-        elif direction1 == turn_right(direction0):
-            winding_number-=1
-
-    winding_number//=4
-
-    interior_direction_list = []
-
-    for command in command_list:
-        direction = direction_dict[command[0]]
-
-        if winding_number == 1:
-            interior_direction_list.append(turn_left(direction))
-        elif winding_number == -1:
-            interior_direction_list.append(turn_right(direction))
-
-    return interior_direction_list
-   
-def build_large_trench(command_list):
-    interior_direction_list = compute_interior_directions(command_list)
-
-
-    trench_dict = {}
-    row_set = set()
-    col_set = set()
-
-    perimeter = 0
-
-    start_coord = (0,0)
-    
-
-    current_coord = start_coord
-    interior_direction = None
-
-    coord_list = [start_coord]
-
-    for i in range(len(command_list)):
-        command = command_list[i]
-
-        dist = command[1]
-
-        perimeter+=dist
-
-        direction = direction_dict[command[0]]
-
-        row_set.add(current_coord[0])
-        col_set.add(current_coord[1])
-
-        current_coord = (current_coord[0]+direction[0]*dist,current_coord[1]+direction[1]*dist)
-
-        coord_list.append(current_coord)
-
-
-    row_list = list(row_set)
-    row_list.sort()
-
-    col_list = list(col_set)
-    col_list.sort()
-
-    
-    index_lookup_dict = {}
-    coord_lookup_dict = {}
-
-    row_zero_index = row_list.index(0)
-    col_zero_index = col_list.index(0)
-
-    for i in range(len(row_list)):
-        for j in range(len(col_list)):
-            index_lookup_dict[(row_list[i],col_list[j])]=(i-row_zero_index,j-col_zero_index)
-
-    for key in index_lookup_dict:
-        val = index_lookup_dict[key]
-        coord_lookup_dict[val] = key
-
-
-    trench_dict_reduced = {}
-
-    for i in range(len(coord_list)-1):
-
-        direction = direction_dict[command_list[i][0]]
-
-        coord0 = coord_list[i]
-        interior_direction0 = interior_direction_list[i]
-
-        reduced_coord0 = index_lookup_dict[coord0]
-
-        if reduced_coord0 not in trench_dict_reduced:
-            trench_dict_reduced[reduced_coord0] = {interior_direction0}
-
-        coord1 = coord_list[i+1]
-        interior_direction1 = interior_direction_list[(i+1)%len(command_list)]
-
-        reduced_coord1 = index_lookup_dict[coord1]
-
-        if reduced_coord1 not in trench_dict_reduced:
-            trench_dict_reduced[reduced_coord1] = {interior_direction0, interior_direction1}
-
-        current_reduced_coord = reduced_coord0
-
-        while current_reduced_coord!=reduced_coord1:
-            current_reduced_coord = (current_reduced_coord[0]+direction[0],current_reduced_coord[1]+direction[1])
-
-            if current_reduced_coord not in trench_dict_reduced:
-                trench_dict_reduced[current_reduced_coord] = {interior_direction0}
-
-
-    return trench_dict_reduced, index_lookup_dict, coord_lookup_dict, perimeter
-
-def build_trench(command_list):
-    interior_direction_list = compute_interior_directions(command_list)
-
-    trench_dict = {}
-    start_coord = (0,0)
-    current_coord = start_coord
-
-    trench_dict[start_coord] = set()
-    interior_direction = None
-
-    for i in range(len(command_list)):
-        command = command_list[i]
-
-        dist = command[1]
-        direction = direction_dict[command[0]]
-
-        interior_direction = interior_direction_list[i]
-
-        if current_coord in trench_dict and i!=0:
-            trench_dict[current_coord].add(interior_direction)
-
-        for i in range(dist):
-            current_coord = (current_coord[0]+direction[0],current_coord[1]+direction[1])
-            trench_dict[current_coord]= {interior_direction}
-
-
-
-    return trench_dict
-
-def trench_boundaries_by_row(trench_dict):
-    row_dict = {}
-
-    for coord in trench_dict:
-        if coord[0] not in row_dict:
-            row_dict[coord[0]] = [coord[1]]
-        else:
-            row_dict[coord[0]].append(coord[1])
-
-    for row_key in row_dict:
-        row_dict[row_key].sort()
-
-    return row_dict
-
-
-
-def solution01():
-    # fname = 'Input01.txt'
-    fname = 'Input02.txt'
-
-    data = parse_input01(fname)
-
-    trench_dict = build_trench(data)
-    row_dict = trench_boundaries_by_row(trench_dict)
-    area = compute_area_by_row(trench_dict,row_dict)
-    print(area)
-    print()
-
-
-    # print(trench_set)
-    # print(data)
+direction_list = [(-1,0),(1,0),(0,-1),(0,1)]
 
 decode_dict = {'0':'R','1':'D','2':'L','3':'U'}
+
 def decode_command(command_in):
     command_str = command_in[2]
     command_str = command_str[2:(len(command_str)-1)]
@@ -229,21 +48,180 @@ def decode_command_list(command_list_in):
 
     return command_list_out
 
+def floodfill_exterior(trench_dict):
+    coord_stack = [(0,0)]
+
+    while len(coord_stack)>0:
+        current_coord = coord_stack.pop(-1)
+
+        trench_dict[current_coord] = 'O'
+
+        for direction in direction_list:
+            next_coord = (current_coord[0]+direction[0],current_coord[1]+direction[1])
+
+            if next_coord in trench_dict and trench_dict[next_coord]=='I':
+                trench_dict[next_coord] = 'O'
+                coord_stack.append(next_coord)
+
+def build_trench(command_list):
+
+    row_set = set()
+    col_set = set()
+
+    start_coord = (0,0)
+    
+    coord_list = [start_coord]
+
+    current_coord = start_coord
+
+
+    for i in range(len(command_list)):
+        command = command_list[i]
+        dist = command[1]
+        direction = direction_dict[command[0]]
+
+        row_set.add(current_coord[0]-1)
+        row_set.add(current_coord[0])
+        row_set.add(current_coord[0]+1)
+
+        col_set.add(current_coord[1]-1)
+        col_set.add(current_coord[1])
+        col_set.add(current_coord[1]+1)
+
+        current_coord = (current_coord[0]+direction[0]*dist,current_coord[1]+direction[1]*dist)
+
+        coord_list.append(current_coord)
+
+
+    row_list = list(row_set)
+    row_list.sort()
+
+    col_list = list(col_set)
+    col_list.sort()
+
+    
+    index_lookup_dict = {}
+    coord_lookup_dict = {}
+
+
+    for i in range(len(row_list)):
+        for j in range(len(col_list)):
+            index_lookup_dict[(row_list[i],col_list[j])]=(i,j)
+
+
+    for key in index_lookup_dict:
+        val = index_lookup_dict[key]
+        coord_lookup_dict[val] = key
+
+
+
+    trench_dict = {}
+    for i in range(len(row_list)):
+        for j in range(len(col_list)):
+            trench_dict[(i,j)]='I'
+
+
+
+    for i in range(len(coord_list)-1):
+        command = command_list[i]
+        dist = command[1]
+        direction = direction_dict[command[0]]
+
+        coord0 = coord_list[i]
+        reduced_coord0 = index_lookup_dict[coord0]
+
+        coord1 = coord_list[i+1]
+        reduced_coord1 = index_lookup_dict[coord1]
+
+        current_reduced_coord = reduced_coord0
+
+        while current_reduced_coord!=reduced_coord1:
+            current_reduced_coord = (current_reduced_coord[0]+direction[0],current_reduced_coord[1]+direction[1])
+
+            trench_dict[current_reduced_coord] = 'B'
+
+    floodfill_exterior(trench_dict)
+
+    return trench_dict, coord_lookup_dict
+
+def compute_square_area(trench_dict, coord_lookup_dict, top_left):
+    bottom_left = (top_left[0]+1,top_left[1])
+    top_right = (top_left[0],top_left[1]+1)
+    bottom_right = (top_left[0]+1,top_left[1]+1)
+
+    top_left_coord = coord_lookup_dict[top_left]
+
+    top_right_coord = None
+    bottom_left_coord = None
+    h = None
+    w = None
+
+    top_left_char = trench_dict[top_left]
+    top_right_char = trench_dict[top_right]
+    bottom_left_char = trench_dict[bottom_left]
+    bottom_right_char = trench_dict[bottom_right]
+
+
+    if bottom_left in coord_lookup_dict:
+        bottom_left_coord = coord_lookup_dict[bottom_left]
+        h = bottom_left_coord[0]-top_left_coord[0]-1
+
+
+    if top_right in coord_lookup_dict:
+        top_right_coord = coord_lookup_dict[top_right]
+        w = top_right_coord[1]-top_left_coord[1]-1
+
+
+    area = 0
+
+    if top_left_char!='O':
+        area+=1
+
+    if top_left_char!='O' and top_right_char!='O':
+        area+=w
+
+    if top_left_char!='O' and bottom_left_char!='O':
+        area+=h
+
+    if top_left_char!='O' and top_right_char!='O' and bottom_left_char!='O' and bottom_right_char!='O':
+        area+=h*w
+
+    return area
+
+def compute_total_area(trench_dict, coord_lookup_dict):
+    area = 0
+    for top_left in trench_dict:
+        if trench_dict[top_left]!='O':
+            area+=compute_square_area(trench_dict, coord_lookup_dict, top_left)
+
+    return area
+
+
+def solution01():
+    # fname = 'Input01.txt'
+    fname = 'Input02.txt'
+
+    data = parse_input01(fname)
+
+    trench_dict, coord_lookup_dict = build_trench(data)
+    area = compute_total_area(trench_dict, coord_lookup_dict)
+    print(area)
+
+
+
+
 def solution02():
-    fname = 'Input01.txt'
-    # fname = 'Input02.txt'
+    # fname = 'Input01.txt'
+    fname = 'Input02.txt'
 
     data = parse_input01(fname)
 
     command_list = decode_command_list(data)
-    trench_dict_reduced, index_lookup_dict, coord_lookup_dict, perimeter = build_large_trench(command_list)
-    row_dict = trench_boundaries_by_row(trench_dict_reduced)
-  
+    trench_dict, coord_lookup_dict = build_trench(command_list)
+    
+    area = compute_total_area(trench_dict, coord_lookup_dict)
+    print(area)
 
-    # for key in trench_dict:
-    #     print(key,trench_dict[key])
-
-    # print(trench_dict)
 
 
 
