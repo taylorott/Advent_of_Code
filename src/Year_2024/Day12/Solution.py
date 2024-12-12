9406de889246069622f915deac814cacfbe0d56c
taylorott <orion.thomas.taylor@gmail.com>
2024-12-12 02:23:05 -0500

    Cleaned up Day12 Solution

--- src/Year_2024/Day12/Solution.py
+++ src/Year_2024/Day12/Solution.py
@@ -24,24 +24,18 @@ def in_bounds(i,j,grid):
 def parse_input01(fname):
     return bh.parse_char_grid(path,fname)
 
-def compute_region_boundary_coords(i,j,c,grid, area_set, boundary_set, visited_set):
-    if (i,j) in area_set:
-        return 0,0
+def dfs_region(i,j,c,grid, area_set, boundary_set):
+    if (i,j) in area_set: return
 
     area_set.add((i,j))
-    visited_set.add((i,j))
-
-    area, perimeter = 1,0
+    grid[i][j] = None
 
     for delta in traversal_dict:
         i_adj,j_adj = i+delta[0], j+delta[1]
         if in_bounds(i_adj,j_adj,grid) and grid[i_adj][j_adj]==c:
-            dA, dP = compute_region_boundary_coords(i_adj,j_adj,c,grid,area_set, boundary_set, visited_set)
-            area, perimeter = area+dA, perimeter+dP
-        else:
-            perimeter+=1
+            dfs_region(i_adj,j_adj,c,grid,area_set, boundary_set)
+        elif (i_adj,j_adj) not in area_set:
             boundary_set.add((i_adj,j_adj,delta))
-    return area, perimeter
 
 def compute_boundary_score(boundary_set):
     total, visited_set = 0, set()
@@ -51,7 +45,7 @@ def compute_boundary_score(boundary_set):
             total, bdry_normal=total+1, coord[2]
 
             for delta in traversal_dict[bdry_normal]:
-                a = 0
+                a = 1
                 while (coord[0]+a*delta[0],coord[1]+a*delta[1],bdry_normal) in boundary_set:
                     visited_set.add((coord[0]+a*delta[0],coord[1]+a*delta[1],bdry_normal))
                     a+=1
@@ -60,16 +54,15 @@ def compute_boundary_score(boundary_set):
 def solution(show_result=True, fname='Input02.txt'):
     grid = parse_input01(fname)
 
-    total1, total2, region_set = 0, 0, set()
+    total1, total2 = 0, 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if (i,j) not in region_set:
-                boundary_set = set()
-                area, perimeter = compute_region_boundary_coords(i,j, grid[i][j], grid,set(), boundary_set, region_set)
+            if grid[i][j] is not None:
+                area_set, boundary_set = set(), set()
+                dfs_region(i,j, grid[i][j], grid,area_set, boundary_set)
                 boundary_score = compute_boundary_score(boundary_set)
-
-                total1+=area*perimeter
-                total2+=area*boundary_score
+                total1+=len(area_set)*len(boundary_set)
+                total2+=len(area_set)*boundary_score
 
     if show_result:
         print(total1)
@@ -81,5 +74,3 @@ if __name__ == '__main__':
     t0 = time.time()
     solution()
     print('runtime in seconds: ','%.3f' % (time.time()-t0))
-    
-
