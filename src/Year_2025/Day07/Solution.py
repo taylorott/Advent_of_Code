1dae7e3d61ed56ac3c06b5ef0aca6900da3a1ab0
taylorott <orion.thomas.taylor@gmail.com>
2025-12-07 11:38:04 -0500

    Day07

--- src/Year_2025/Day07/Solution.py
+++ src/Year_2025/Day07/Solution.py
@@ -16,36 +16,77 @@ from functools import cmp_to_key
 path = currentdir
 
 def parse_input01(fname):
-    data = None
-    
-    # data = bh.parse_num_column(path,fname)
-    # data = bh.parse_digit_grid(path,fname)
-    # data = bh.parse_char_grid(path,fname)
-    # data = bh.parse_split_by_emptylines(path,fname,delimiters = [],type_lookup = None, allInt = False, allFloat = False)
-    # data = bh.parse_strings(path,fname,delimiters = [],type_lookup = None, allInt = False, allFloat = False)
-    # data = bh.parse_extract_ints_split_by_emptylines(path,fname)
-    # data = bh.parse_extract_ints(path,fname)
+    data = bh.parse_char_grid(path,fname)
 
     return data
 
-def solution01():
-    fname = 'Input01.txt'
-    # fname = 'Input02.txt'
+def update_line(grid,k):
+    total = 0
 
+    width = len(grid[0])
+    for i in range(width):
+        if grid[k][i]=='.':
+            test0 = grid[k-1][i] == 'S' or grid[k-1][i] == '|' 
+            test1 =  0<i and grid[k][i-1]=='^' and grid[k-1][i-1]=='|'
+            test2 =  i<width-1 and grid[k][i+1]=='^' and grid[k-1][i+1]=='|'
+
+            if test0 or test1 or test2:
+                grid[k][i]='|'
+
+        if grid[k][i]=='^' and (grid[k-1][i]=='|' or grid[k-1][i]=='S'):
+            total+=1
+
+    return total
+
+def update_grid(grid):
+    total = 0
+    for i in range(1,len(grid)):
+        total+=update_line(grid,i)
+    return total
+
+def update_timelines(grid,timelines,k):
+    total = 0
+    width = len(grid[0])
+
+    for i in range(width):
+        if grid[k][i]=='|' or grid[k][i]=='S':
+            if grid[k+1][i]=='|':
+                timelines[k][i]=timelines[k+1][i]
+            if grid[k+1][i]=='^':
+                if 0<i and grid[k+1][i-1]=='|':
+                    timelines[k][i]+=timelines[k+1][i-1]
+                if i<width-1 and grid[k+1][i+1]=='|':
+                    timelines[k][i]+=timelines[k+1][i+1]
+            total+=timelines[k][i]
+    return total
+
+def solution(show_result=True, fname='Input02.txt'):
     data = parse_input01(fname)
-    print(data)
+    val1 = update_grid(data)
 
-def solution02():
-    fname = 'Input01.txt'
-    # fname = 'Input02.txt'
+    h, w = len(data), len(data[0])
 
-    # data = parse_input01(fname)
+    timelines = []
+    for i in range(h):
+        timelines.append([0]*w)
 
+    for i in range(w):
+        if data[-1][i] == '|':
+            timelines[-1][i] = 1
+
+    val2 = None
+    for i in range(h-2,-1,-1):
+        val2 = update_timelines(data,timelines,i)
+
+    if show_result:
+        print(val1)
+        print(val2)
+
+    return val1, val2
 
 if __name__ == '__main__':
     t0 = time.time()
-    solution01()
-    solution02()
+    solution()
     # print('runtime in seconds: ','%.3f' % (time.time()-t0))
     
 
