f63ebd5fd08090992aac247bf6ecb4d6c2f4f1c5
taylorott <orion.thomas.taylor@gmail.com>
2025-12-09 09:51:20 -0500

    Day09

--- src/Tests/Year_2025_tests.py
+++ src/Tests/Year_2025_tests.py
@@ -20,23 +20,10 @@ from Year_2025.Day05 import Solution as Day05
 from Year_2025.Day06 import Solution as Day06
 from Year_2025.Day07 import Solution as Day07
 from Year_2025.Day08 import Solution as Day08
-# from Year_2025.Day09 import Solution as Day09
+from Year_2025.Day09 import Solution as Day09
 # from Year_2025.Day10 import Solution as Day10
 # from Year_2025.Day11 import Solution as Day11
 # from Year_2025.Day12 import Solution as Day12
-# from Year_2025.Day13 import Solution as Day13
-# from Year_2025.Day14 import Solution as Day14
-# from Year_2025.Day15 import Solution as Day15
-# from Year_2025.Day16 import Solution as Day16
-# from Year_2025.Day17 import Solution as Day17
-# from Year_2025.Day18 import Solution as Day18
-# from Year_2025.Day19 import Solution as Day19
-# from Year_2025.Day20 import Solution as Day20
-# from Year_2025.Day21 import Solution as Day21
-# from Year_2025.Day22 import Solution as Day22
-# from Year_2025.Day23 import Solution as Day23
-# from Year_2025.Day24 import Solution as Day24
-# from Year_2025.Day25 import Solution as Day25
 
 path = currentdir
 
@@ -200,6 +187,22 @@ def Day09_test():
     current_day = Day09
     day_label = 'Day09'
 
+    fname = 'Input01.txt'
+    v1 = current_day.solution01(show_result=False, fname=fname)
+    v2 = current_day.solution02(show_result=False, fname=fname)
+    v1_true, v2_true = 50, 24
+
+    assert v1==v1_true, day_label+", Part 1 Failed, Input: "+fname
+    assert v2==v2_true, day_label+", Part 2 Failed, Input: "+fname
+
+    fname = 'Input02.txt'
+    v1 = current_day.solution01(show_result=False, fname=fname)
+    v2 = current_day.solution02(show_result=False, fname=fname)
+    v1_true, v2_true = 4750297200, 1578115935
+
+    assert v1==v1_true, day_label+", Part 1 Failed, Input: "+fname
+    assert v2==v2_true, day_label+", Part 2 Failed, Input: "+fname
+
 def Day10_test():
     current_day = Day10
     day_label = 'Day10'
@@ -212,58 +215,6 @@ def Day12_test():
     current_day = Day12
     day_label = 'Day12'
 
-def Day13_test():
-    current_day = Day13
-    day_label = 'Day13'
-
-def Day14_test():
-    current_day = Day14
-    day_label = 'Day14'
-
-def Day15_test():
-    current_day = Day15
-    day_label = 'Day15'
-
-def Day16_test():
-    current_day = Day16
-    day_label = 'Day16'
-
-def Day17_test():
-    current_day = Day17
-    day_label = 'Day17'
-
-def Day18_test():
-    current_day = Day18
-    day_label = 'Day18'
-
-def Day19_test():
-    current_day = Day19
-    day_label = 'Day19'
-
-def Day20_test():
-    current_day = Day20
-    day_label = 'Day20'
-
-def Day21_test():
-    current_day = Day21
-    day_label = 'Day21'
-
-def Day22_test():
-    current_day = Day22
-    day_label = 'Day22'
-
-def Day23_test():
-    current_day = Day23
-    day_label = 'Day23'
-
-def Day24_test():
-    current_day = Day24
-    day_label = 'Day24'
-
-def Day25_test():
-    current_day = Day25
-    day_label = 'Day25'
-    
 
 def run_all_tests():
     t0 = time.time()
@@ -314,11 +265,11 @@ def run_all_tests():
     print('test runtime in seconds: ','%.3f' % (time.time()-t0))
     print('')
 
-    # t0 = time.time()
-    # print('Day09 Test:')
-    # Day09_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
+    t0 = time.time()
+    print('Day09 Test:')
+    Day09_test()
+    print('test runtime in seconds: ','%.3f' % (time.time()-t0))
+    print('')
 
     # t0 = time.time()
     # print('Day10 Test:')
@@ -337,84 +288,7 @@ def run_all_tests():
     # Day12_test()
     # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
     # print('')
-    
-    # t0 = time.time()
-    # print('Day13 Test:')
-    # Day13_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
 
-    # t0 = time.time()
-    # print('Day14 Test:')
-    # Day14_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day15 Test:')
-    # Day15_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day16 Test:')
-    # Day16_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day17 Test:')
-    # Day17_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day18 Test:')
-    # Day18_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day19 Test:')
-    # Day19_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day20 Test:')
-    # Day20_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day21 Test:')
-    # Day21_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day22 Test:')
-    # Day22_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day23 Test:')
-    # Day23_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day24 Test:')
-    # Day24_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
-
-    # t0 = time.time()
-    # print('Day25 Test:')
-    # Day25_test()
-    # print('test runtime in seconds: ','%.3f' % (time.time()-t0))
-    # print('')
 
 if __name__ == '__main__':
     t0 = time.time()
--- src/Year_2025/Day09/Solution.py
+++ src/Year_2025/Day09/Solution.py
@@ -16,30 +16,173 @@ from functools import cmp_to_key
 path = currentdir
 
 def parse_input01(fname):
-    data = None
-    
-    # data = bh.parse_num_column(path,fname)
-    # data = bh.parse_digit_grid(path,fname)
-    # data = bh.parse_char_grid(path,fname)
-    # data = bh.parse_split_by_emptylines(path,fname,delimiters = [],type_lookup = None, allInt = False, allFloat = False)
-    # data = bh.parse_strings(path,fname,delimiters = [],type_lookup = None, allInt = False, allFloat = False)
-    # data = bh.parse_extract_ints_split_by_emptylines(path,fname)
-    # data = bh.parse_extract_ints(path,fname)
+    data = bh.parse_strings(path,fname,delimiters = [','],type_lookup = None, allInt = True, allFloat = False)
 
     return data
 
-def solution01():
-    fname = 'Input01.txt'
-    # fname = 'Input02.txt'
+def compute_area(c1,c2):
+    dx = abs(c1[0]-c2[0])+1
+    dy = abs(c1[1]-c2[1])+1
+    return dx*dy
 
+def compute_area_transformed_coords(coord1,coord2,x_lookup,y_lookup):
+    return compute_area(transform_coord(coord1,x_lookup,y_lookup),
+                        transform_coord(coord2,x_lookup,y_lookup))
+
+def transform_coord(coord,x_lookup,y_lookup):
+    return (x_lookup[coord[0]],y_lookup[coord[1]])
+
+def compute_winding(coord_list):
+    n = len(coord_list)
+    total = 0
+    for i in range(n):
+        j = (i+1)%n
+        k = (i+2)%n
+
+        dx1 = coord_list[j][0]-coord_list[i][0]
+        dy1 = coord_list[j][1]-coord_list[i][1]
+
+        dx2 = coord_list[k][0]-coord_list[j][0]
+        dy2 = coord_list[k][1]-coord_list[j][1]
+
+
+        total+=np.sign(dx1*dy2-dy1*dx2)
+
+    return round(total/4)
+
+def construct_coord_lookups(coord_list):
+    x_set, y_set = set(), set()
+    
+    for coord in coord_list:
+        for i in range(-1,2):
+            x_set.add(coord[0]+i)
+            y_set.add(coord[1]+i)
+
+    x_forward, y_forward = sorted(list(x_set)), sorted(list(y_set))
+    
+    x_reverse, y_reverse = dict(), dict()
+
+    n = len(coord_list)
+
+    for i in range(len(x_forward)):
+        x_reverse[x_forward[i]] = i
+
+    for i in range(len(y_forward)):
+        y_reverse[y_forward[i]] = i
+
+    coord_list_transformed = []
+    for coord in coord_list:
+        coord_list_transformed.append(transform_coord(coord,x_reverse,y_reverse))
+
+    return x_forward, x_reverse, y_forward, y_reverse, coord_list_transformed
+
+delta_list = [(1,0),(-1,0),(0,1),(0,-1)]
+
+def flood_fill(start_coord,fill_set):
+    todo_list = [start_coord]
+
+    while len(todo_list)>0:       
+        coord = todo_list.pop()
+        fill_set.add(coord)
+
+        for delta in delta_list:
+            next_coord = (coord[0]+delta[0],coord[1]+delta[1])
+
+            if next_coord not in fill_set:
+                todo_list.append(next_coord)
+
+def run_flood_fill(coord_list,fill_set):
+    winding = compute_winding(coord_list)
+
+    n = len(coord_list)
+    for i in range(n):
+        j = (i+1)%n
+        k = (i+2)%n
+
+        dx1 = coord_list[j][0]-coord_list[i][0]
+        dy1 = coord_list[j][1]-coord_list[i][1]
+        dx2 = coord_list[k][0]-coord_list[j][0]
+        dy2 = coord_list[k][1]-coord_list[j][1]
+
+        turn = np.sign(dx1*dy2-dy1*dx2)
+
+        if turn == winding:
+            x = coord_list[j][0]+np.sign(dx2)-np.sign(dx1)
+            y = coord_list[j][1]+np.sign(dy2)-np.sign(dy1)
+
+            flood_fill((x,y),fill_set)
+
+def directional_increment(coord,coord_target):
+    x = coord[0]+np.sign(coord_target[0]-coord[0])
+    y = coord[1]+np.sign(coord_target[1]-coord[1])
+    return (x,y)
+
+
+def solution01(show_result=True, fname='Input02.txt'):
     data = parse_input01(fname)
-    print(data)
 
-def solution02():
-    fname = 'Input01.txt'
-    # fname = 'Input02.txt'
+    area = 0
+    for i in range(len(data)):
+        for j in range(i+1,len(data)):
+            area = max(area,compute_area(data[i],data[j]))
+    
+    if show_result:
+        print(area)
+    return area
 
-    # data = parse_input01(fname)
+def solution02(show_result=True, fname='Input02.txt'):
+    data = parse_input01(fname)
+
+    x_forward, x_reverse, y_forward, y_reverse, coord_list_transformed = construct_coord_lookups(data)
+
+    n = len(data)
+
+    fill_set = set()
+
+    for i in range(n):
+        coord = coord_list_transformed[i]
+        coord_target = coord_list_transformed[(i+1)%n]
+
+        fill_set.add(coord)
+        while coord!=coord_target:
+            coord = directional_increment(coord,coord_target)
+            fill_set.add(coord)
+
+    run_flood_fill(coord_list_transformed,fill_set)
+
+    bottom_dict = dict()
+    for i in range(len(x_forward)):
+        bottom_most_coord = None
+        for j in range(len(y_forward)):
+            if (i,j) in fill_set:
+                if bottom_most_coord is None:
+                    bottom_most_coord = j
+                bottom_dict[(i,j)] = bottom_most_coord
+            else:
+                bottom_most_coord = None
+                bottom_dict[(i,j)] = np.inf
+
+    area = 0
+    for i in range(len(coord_list_transformed)):
+        coord1 = coord_list_transformed[i]
+        for j in range(i+1,len(coord_list_transformed)):
+            coord2 = coord_list_transformed[j]
+
+            y_bottom = min(coord1[1],coord2[1])
+            y_top = max(coord1[1],coord2[1])
+
+            is_full = True
+            for x in range(min(coord1[0],coord2[0]),max(coord1[0],coord2[0])+1):
+                if bottom_dict[(x,y_top)]>y_bottom:
+                    is_full = False
+                    break
+
+            if is_full:
+                area = max(area,compute_area_transformed_coords(coord1,coord2,x_forward,y_forward))
+
+    if show_result:
+        print(area)
+    return area
 
 
 if __name__ == '__main__':
